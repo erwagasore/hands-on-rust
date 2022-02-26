a21912582fb697feda088ffe97cf325ac76c80fa
Eugene Rwagasore <rwagasore@gmail.com>
2022-02-26 11:56:47 +0100

    Completed basic flappy dragon

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,122 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point {
+            x,
+            y,
+        }
+    }
+}
+
+struct Player {
+    point: Point,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(point: Point) -> Self {
+        Player {
+            point,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.point.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.point.y += self.velocity as i32;
+        self.point.x += 1;
+        if self.point.y < 0 {
+            self.point.y = 0;
+        }
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score)
+        }
+    }
+
+    fn got_hit(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.point.x == self.x;
+        let player_above_gap = player.point.y < self.gap_y - half_size;
+        let player_below_gap = player.point.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(Point::new(5, 25)),
+            frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0
         }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(Point::new(5, 25));
+        self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -36,14 +139,39 @@ impl State {
         }
     }
 
-    fn playing(&mut self, _ctx: &mut BTerm) {
-        // TODO: fill-in the stub later
-        self.mode = GameMode::End;
+    fn playing(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.point.x);
+
+        if self.player.point.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.point.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.point.y > SCREEN_HEIGHT || self.obstacle.got_hit(&self.player) {
+            self.mode = GameMode::End;
+        }
     }
 
     fn dead(&mut self, ctx:&mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
 
@@ -69,7 +197,7 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::simple(SCREEN_WIDTH, SCREEN_HEIGHT)?
         .with_title("Flappy Dragon")
         .build()?;
 
